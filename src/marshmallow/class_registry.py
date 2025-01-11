10332c59831c763d1ff7c7f92db9bb8ca0c5a386
Steven Loria <sloria1@gmail.com>
2025-01-11 18:34:35 -0500

    Fix a few type annotations in fields and class_registry (#2756)
    * Fix a few type annotations in fields and class_registry
    
    * Update changelog

--- src/marshmallow/class_registry.py
+++ src/marshmallow/class_registry.py
@@ -74,15 +74,13 @@ def get_class(classname: str, all: typing.Literal[False] = ...) -> SchemaType: .
 
 
 @typing.overload
-def get_class(
-    classname: str, all: typing.Literal[True] = ...
-) -> list[SchemaType] | SchemaType: ...
+def get_class(classname: str, all: typing.Literal[True] = ...) -> list[SchemaType]: ...
 
 
 def get_class(classname: str, all: bool = False) -> list[SchemaType] | SchemaType:
     """Retrieve a class from the registry.
 
-    :raises: marshmallow.exceptions.RegistryError if the class cannot be found
+    :raises: `marshmallow.exceptions.RegistryError` if the class cannot be found
         or if there are multiple entries for the given class name.
     """
     try:
